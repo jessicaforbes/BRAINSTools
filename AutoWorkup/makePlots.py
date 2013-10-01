eb03c26b2989bcc0d18b6b14d54b53000f895a32
Jessica Forbes <jessica-forbes@uiowa.edu>
2013-10-01 16:31:15 -0500

    ENH: Added function to plot a scatter plot for each project with gradients binned every 5 degrees in phi and theta, circles of varying widths shown based on relative number of gradients in each bin, and color varying based on the percentage of good gradients in each bin.

--- AutoWorkup/makePlots.py
+++ AutoWorkup/makePlots.py
@@ -11,8 +11,70 @@ class MakePlots():
         self.tableName = "dtiprep"
 
     def main(self):
-        self.plotPerSite()
-        self.plotPerSitePerYear()
+        # self.plotPerSite()
+        # self.plotPerSitePerYear()
+        self.scatterPlot()
+
+    def scatterPlot(self):
+        path = os.path.join("test_SCATTER_{0}_Plots_By_Site_DTIPrep_Output_XML.pdf".format(datetime.date.today().isoformat()))
+        pp = pdfpages(path)
+        site_list = self.getListFromDB("SELECT DISTINCT project FROM {} ORDER BY project;".format(self.tableName))
+        for site in site_list:
+            bins = self.getBins(site)
+            (x, y, percentGoodList, size) = self.makePlotLists(bins)
+            print x, y, percentGoodList, size
+            print
+            self.setPlotAttributes('Processing Type at Point Phi Vs. Theta \nfor Project {0}\n'.format(site))
+            plt.scatter(x, y, s=size, c=percentGoodList)
+            print percentGoodList
+            # plt.show()
+            pp.savefig()
+            plt.close()
+        pp.close()
+
+    def makePlotLists(self, bins):
+        x = list()
+        y = list()
+        percentGoodList = list()
+        size = list()
+        for (key,item) in sorted(bins.items()):
+            x.append(int(key[0]))
+            y.append(int(key[1]))
+            (total, percentGood) = self.getStats(item)
+            percentGoodList.append(percentGood)
+            size.append(total)
+        return x, y, percentGoodList, size
+
+    def getStats(self,item):
+        good = float(0)
+        bad = float(0)
+        for val in item:
+            if val == "EDDY_MOTION_CORRECTED":
+                good += 1
+            elif "EXCLUDE_" in val:
+                bad += 1
+            elif val == "BASELINE_AVERAGED":
+                pass
+            else:
+                print "ERROR: Processing type not known: {}".format(val)
+        total = good + bad
+        if total == 0:
+            percentGood = 0
+        else:
+            percentGood = good / total
+        return total, percentGood
+
+    def getBins(self, site):
+        stepSize = 5
+        xPoints = range(-180, 180, stepSize)
+        yPoints = range(0, 90, stepSize)
+        bins = dict()
+        for xval in xPoints:
+            for yval in yPoints:
+                bins[(xval, yval)] = self.getListFromDB("SELECT processing FROM {} WHERE "
+                    "phi>={} AND phi<{} AND theta>={} AND theta<{} AND project='{}';".format(
+                    self.tableName,xval,xval+stepSize,yval,yval+stepSize,site))
+        return bins
 
     def _querySQLiteDB(self, SQLite_query):
         con = lite.connect(self.dbFileName)
@@ -27,7 +89,7 @@ class MakePlots():
         List = list()
         # the project from the database is stored in a tuple EX: (u'Project', )
         for row in query_results:
-            list_item = row[0]
+            list_item = str(row[0])
             List.append(list_item)
         return List
 
@@ -83,12 +145,8 @@ class MakePlots():
         plt.ylabel("Theta (degrees) \n", fontsize = 'large')
         plt.axis([-180, 180, 0, 90])
         plt.subplots_adjust(bottom = 0.2, top = 0.86, right = .88, left = 0.15)
-        plt.legend(fontsize = 'small', bbox_to_anchor=(0., -.27, 1., .02), loc=3,
-              ncol=2, mode="expand", borderaxespad=0.,shadow=True)
-
-    def plotPerProcessingPerSite(self):
-        path = os.path.join("{0}_Processing_Plots_By_Site_DTIPrep_Output_XML.pdf".format(datetime.date.today().isoformat()))
-        pp = pdfpages(path)
+        # plt.legend(fontsize = 'small', bbox_to_anchor=(0., -.27, 1., .02), loc=3,
+        #       ncol=2, mode="expand", borderaxespad=0.,shadow=True)
 
     def getProcessingDict(self, site, year=None):
         if year == None:
